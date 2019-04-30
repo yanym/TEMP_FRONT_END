776b13570e106e0931fbfa014650ee1a6b0abcd6
Yiming <ymyan6@gmail.com>
2019-04-29 20:51:25 -0400

    Build

---
+++ build/precache-manifest.d3eb55e31ab319b58bd5d7d2a70db360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e82808beffe9119f3296c7fbf6581f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4233eaf3c11460a80ad",
+    "url": "/static/css/2.2ba631aa.chunk.css"
+  },
+  {
+    "revision": "1054853dc4171dc801c2",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "d4233eaf3c11460a80ad",
+    "url": "/static/js/2.5d743c9c.chunk.js"
+  },
+  {
+    "revision": "1054853dc4171dc801c2",
+    "url": "/static/js/main.75629592.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
